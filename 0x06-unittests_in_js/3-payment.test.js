e5d5a1f641218ba59ff0c2df60e5c0e23e1e9dae
iceking-fct <miracleamajama14@gmail.com>
2025-01-07 13:04:26 +0100

    task 3; "3-payment.js" solved

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const sendPayment = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('sendPaymentRequest uses Utils.calculateNumber', () => {
+    const sendPaymentSpy = sinon.spy(sendPayment);
+    const calculateSpy = sinon.spy(Utils, 'calculateNumber');
+    sendPayment(100, 20);
+    expect(sendPaymentSpy.calledOnce);
+    expect(calculateSpy.calledOnce);
+    expect(sendPaymentSpy.calledOnceWith(100, 20));
+    expect(calculateSpy.calledAfter(sendPaymentSpy));
+    expect(calculateSpy.calledWith('SUM', 100, 20));
+  });
+ });
